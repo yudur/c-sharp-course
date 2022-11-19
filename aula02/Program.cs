891d8d9742d52c88c15d3d4f7a55b95fb6a9c15d
yudur <duarteyudi218@gmail.com>
2022-11-19 14:05:54 -0300

    first commit

---
+++ aula02/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("hello word");
+        if(args.GetLength(0)>0){
+            Console.WriteLine(args.GetValue(0));
+        }
+    }
+}
